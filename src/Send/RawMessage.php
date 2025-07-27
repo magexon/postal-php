c8703cf1bd4f32eaa2617ec7ebb9a330a629d612
Maxexon <89267831+magexon@users.noreply.github.com>
2025-07-27 02:20:20 +0100

    Add files via upload

---
+++ src/Send/RawMessage.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Postal\Send;
+
+class RawMessage
+{
+    /**
+     * @var array<string>
+     */
+    public array $rcpt_to = [];
+
+    public ?string $mail_from = null;
+
+    public ?string $data = null;
+
+    public function mailFrom(string $address): self
+    {
+        $this->mail_from = $address;
+
+        return $this;
+    }
+
+    public function rcptTo(string $address): self
+    {
+        $this->rcpt_to[] = $address;
+
+        return $this;
+    }
+
+    public function data(string $data): self
+    {
+        $this->data = base64_encode($data);
+
+        return $this;
+    }
+}
